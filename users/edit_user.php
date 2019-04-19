c361bcf096e98fdf538fff43f57b3ba1291023b4
abhinavRai23 <raiabhinavrai1994@gmail.com>
2019-04-19 19:34:57 +0530

    add n view user

---
+++ users/edit_user.php
@@ -0,0 +1,71 @@
+<?php
+    ob_start();
+    include "../include/header.php";
+?>
+<h1 class="page-header">
+    Edit User Details
+</h1>
+<div class="col-md-4 col-md-offset-4">
+    <div class="panel panel-default">
+        <!-- /.panel-heading -->
+        <div class="panel-body">
+            <form role="form" action="edit_user.php" method="post">
+                <fieldset>
+                    <?php
+                    if(isset($_POST["user_id"]) && isset($_POST["username"]) && isset($_POST["contact_no"]) && isset($_POST["email_id"]))
+                    {
+                        $user_id= mysql_entities_fix_string($db_server, $_POST["user_id"]);
+                        $username = mysql_entities_fix_string($db_server, $_POST["username"]);
+                        $contact_no = mysql_entities_fix_string($db_server, $_POST["contact_no"]);
+                        $email_id = mysql_entities_fix_string($db_server, $_POST["email_id"]);
+
+                        $query = "UPDATE user SET username='$username', contact_no='$contact_no', email_id='$email_id' WHERE user_id=$user_id ";
+                        $run = mysqli_query($db_server, $query);
+
+                        if(!$run) die (mysqli_error($db_server));
+                        else{
+                            header("location:view_user.php");
+                        }
+                    }
+                    else if(isset($_GET['user']))
+                    {
+                        $user_id= htmlentities($_GET['user']);
+                        $query = "SELECT * FROM `user` where user_id='$user_id'";
+                        $run = mysqli_query($db_server, $query);
+
+                        if(!$run) die (mysqli_error($db_server));
+
+                        $rows=mysqli_num_rows($run);
+
+                        if($rows>0)
+                        {
+                            $row = mysqli_fetch_assoc($run);
+                        }
+                    }
+                    else{
+                        header("location:view_user.php");
+                    }
+                    ?>
+                    <input type='hidden' name='user_id' value='<?php echo $row["user_id"]?>'>
+                    <div class="form-group">
+                        Username:<input class="form-control" value="<?php echo $row["username"]; ?>" name="username"
+                            type="varchar" required autofocus="on">
+                    </div>
+                    <div class="form-group">
+                        Contact no:<input class="form-control" value="<?php echo $row["contact_no"]; ?>" name="contact_no"
+                            type="number" required>
+                    </div>
+                    <div class="form-group">
+                        Email id:<input class="form-control" value="<?php echo $row["email_id"]; ?>" name="email_id" type="varchar" required>
+                    </div>
+                    <input type="Submit" value="Submit">
+                </fieldset>
+            </form>
+
+        </div>
+        <!-- /.panel-body -->
+    </div>
+</div>
+<?php 
+    include "../include/footer.php";
+?>
---
+++ users/view_user.php
@@ -0,0 +1,61 @@
+<?php
+    include "../include/header.php";
+?>
+<h1 class="page-header">
+    All User
+</h1>
+
+<div class="panel panel-default">
+    <!-- /.panel-heading -->
+    <div class="panel-body">
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Contact No</th>
+                        <th>Email id</th>
+                        <th>Edit</th>
+                    </tr>
+                </thead>
+
+        <?php
+
+        $query = "SELECT user_id, username, contact_no, email_id FROM `user` where user_type=0 ORDER BY `username` ASC";
+		$run = mysqli_query($db_server, $query);
+
+		if(!$run) die (mysqli_error($db_server));
+
+        $rows=mysqli_num_rows($run);
+		echo "<tbody>";
+		if($rows!=0)
+		{
+			for ($j = 0 ; $j < $rows ; ++$j)
+			{
+                $row = mysqli_fetch_assoc($run);
+				echo "
+				<tr>
+                    <td>".($j+1)."</td>
+                    <td>".$row["username"]."</td>
+                    <td>".$row["contact_no"]."</td>
+                    <td>".$row["email_id"]."</td>
+                    <td>
+                        <a href='edit_user.php?user=".$row["user_id"]."' class='btn btn-success'>Edit</a>
+                    </td>
+                </tr>
+				";
+			}	
+		}
+        ?>        
+                </tbody>
+            </table>
+        </div>
+        <!-- /.table-responsive -->
+    </div>
+    <!-- /.panel-body -->
+</div>
+
+<?php
+    include "../include/footer.php";
+?>
