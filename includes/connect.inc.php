2d1b0ca1ba70549420ad0ae71d9cc1e67bf5ab98
abhinavRai23 <raiabhinavrai1994@gmail.com>
2019-05-12 15:29:52 +0530

    prod config added

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -1,8 +1,9 @@
 <?php
-	$db_hostname = 'localhost';
-	$db_database = 'vinra';
-	$db_username =  'root';
-	$db_password = '';
+	$prod = false;
+	$db_hostname = $prod ? '182.50.133.88:3306' : 'localhost';
+	$db_database = $prod ? 'ph16722039727_vinra' : 'vinra';
+	$db_username =  $prod ? 'vinra' : 'root';
+	$db_password = $prod ? 'Mo&p6a97' : '';
 	$db_server = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
-	if (!$db_server) die("Unable to connect to MySQL: ". mysqli_error($db_server));
+	if (!$db_server) die("Unable to connect to MySQL: " . mysqli_error($db_server));
 ?>
