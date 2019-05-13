4a6161d8c3e747a1d8724b4868ecc80597e663dc
abhinavRai23 <raiabhinavrai1994@gmail.com>
2019-05-14 00:54:47 +0530

    view perticular book and sample pdf view

--- buy-online-books.php
+++ buy-online-books.php
@@ -1,7 +1,22 @@
 <?php
     include "includes/header.php";
-?>
 
+    if(isset($_GET['isbn'])){
+        $isbn = mysql_entities_fix_string($db_server, $_GET["isbn"]);
+        $query = "SELECT * FROM `books` where isbn='$isbn'";
+        $run = mysqli_query($db_server, $query);
+
+        if(!$run) die (mysqli_error($db_server));
+
+        $rows=mysqli_num_rows($run);
+        if($rows!=0){
+            $row = mysqli_fetch_assoc($run);
+        }
+    }
+    else{
+        header("location:view_books.php");
+    }
+?>
 
 <div class="container">
     <hr class="line">
@@ -9,60 +24,22 @@
         <div class="row">
             <div class="row book-desc-bg">
                 <div class="col-md-3 pad-top20">
-                    <a href="">
-                        <img src="Images/124.jpeg" height="240px" width="180px" />
-                    </a>
+                    <img src="/uploads/poster/<?php echo $row["poster"]; ?>" height="240px" width="180px" />
                 </div>
                 <div class="col-md-9 pad-top20">
-                    <h4>RAW (Romeo Akbar Walter)</h4>
-                    <h5>Author Name</h5>
-                    <p>Publisher Name</p>
-                    <p><span class=" pad-right10">ISBN NO.</span> | <span class="pad-left10 pad-right10"> 336 Pages
-                        </span> | <span class="pad-left10 pad-right10">Rs. 350 </span> | <span
-                            class="pad-left10 pad-right10">2019</span></p>
-                    <p>This is the sample of book description. this is sample text to fill out the empty
-                        space.
-                        This is the sample of book description. this is sample text to fill out the empty
-                        space. This is the sample of book description. this is sample text to fill out the
-                        empty space. This is the sample of book description. this is sample text to fill
-                        out
-                        the empty space.</p>
+                    <h4><?php echo $row["title"]; ?></h4>
+                    <h5><?php echo $row["author"]; ?></h5>
+                    <p><?php echo $row["publisher_name"]; ?></p>
+                    <p><span class=" pad-right10">ISBN NO. <?php echo $row["isbn"]; ?></span> | <span class="pad-left10 pad-right10"> <?php echo $row["no_of_pages"]; ?> Pages
+                        </span> | <span class="pad-left10 pad-right10">Rs. <?php echo $row["price"]; ?></span> | <span
+                            class="pad-left10 pad-right10"><?php echo $row["published_date"]; ?></span></p>
+                    <p><?php echo $row["book_desc"]; ?></p>
                     <div class="contact-sec-btn center" style="display: inline-block;">Buy Now</div>
-                    <div class="contact-sec-btn center" style="display: inline-block;">View Sample PDF</div>
+                    <div class="contact-sec-btn center" style="display: inline-block;"><a href="/uploads/sample_pdf_book/<?php echo $row["sample_pdf_book"]; ?>" target="_blank" style="color: white; text-decoration: none">View Sample PDF</a></div>
                     <hr class="line">
 
                 </div>
-                <div class="row">
-                    <div class="col-md-12 col-sm-12">
-                        <ul class="nav nav-tabs">
-                            <li class="active"><a data-toggle="tab" href="#home">Content</a></li>
-                            <li><a data-toggle="tab" href="#menu1">New in this edition</a></li>
-                            <li><a data-toggle="tab" href="#menu2">Salient Features</a></li>
-                        </ul>
-
-                        <div class="tab-content">
-                            <div id="home" class="tab-pane fade in active">
-                                <h3>Content of book</h3>
-                                <p> This is the sample of book description. this is sample text to fill out the empty
-                                    space. This is the sample of book description. this is sample text to fill out.</p>
-                            </div>
-                            <div id="menu1" class="tab-pane fade">
-                                <h3>Keyword</h3>
-                                <p>1. Chapter on Technology Powered Communication</p>
-                                <p>2. Chapter on Soft Skills in Business</p>
-                                <p>3. Sections on eleven new topics</p>
-                            </div>
-                            <div id="menu2" class="tab-pane fade">
-                                <h3>New Features</h3>
-                                <p>May be this book have some silent feature. we forgot to put in book.</p>
-                            </div>
-                        </div>
-
-                    </div><br />
-                </div><br /><br /><br /><br />
             </div>
-
-
         </div>
     </div>
 </div>
